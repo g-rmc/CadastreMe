14216b62fec52cf63b766c71fb35ec473227e7ee
g-rmc <g.rmc3000@gmail.com>
2023-03-16 14:59:55 -0300

    fix: button interact when disbaled

--- src/components/utils/Button.js
+++ src/components/utils/Button.js
@@ -16,12 +16,24 @@ export function CustomButton({message, action, navigateTo, disabled}) {
     )
 }
 
+const disabledButtonStyle = `
+    background-color: #9C9C9C;
+    cursor: default;
+    :hover{
+        filter: brightness(1.0);
+        filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    }
+    :active{
+        transform: none;
+    }
+`
+
 const Container = styled.button`
     width: 100%;
     height: 40px;
     font-size: 24px;
     color: white;
-    ${props => props.disabled? "background-color: #9C9C9C;" : "background-color: #21480B;"}
+    ${props => props.disabled? disabledButtonStyle : "background-color: #21480B;"}
     border-radius: 10px;
     align-items: center;
     justify-content: center;
