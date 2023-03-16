4915b4398c54dcda422767252a3daad3b4cd80ea
g-rmc <g.rmc3000@gmail.com>
2023-03-16 12:30:28 -0300

    feat: add viacep service

---
+++ src/services/viacep.js
@@ -0,0 +1,14 @@
+//appname.js
+
+import axios from 'axios';
+
+function generateViaCepUrl (cep) {
+    if (isNaN(cep)) throw new Error('CEP must be a number!')
+    if (cep.toString().length !== 8) throw new Error('Invalid CEP length!')
+    return `https://viacep.com.br/ws/${cep}/json/`
+}
+
+export function getCEPinfo (cep){
+    const promise = axios.get(generateViaCepUrl(cep));
+    return promise;
+}
